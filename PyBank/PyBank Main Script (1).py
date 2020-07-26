d5fefcc0ac981cf4b892cb6016023534c5d0bf29
wavyguy <66179374+wavyguy@users.noreply.github.com>
2020-07-25 23:26:27 -0500

    main.py

---
+++ PyBank/PyBank Main Script (1).py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import os
+import csv
+from pathlib import Path
+
+
+# In[3]:
+
+
+csvpath = os.path.join('..', 'Resources', 'budgetdata.csv')
+
+
+# In[4]:
+
+
+#Empty List
+month = []
+revenue =[]
+revenue_change = []
+
+#Read CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    
+   #Iterate Through Rows in File 
+    for row in csvreader:
+        month.append(row[0])
+        revenue.append(int(row[1]))
+
+
+# In[5]:
+
+
+#Monthly Change in Revenue
+for i in range(len(revenue)-1):
+    revenue_change.append(revenue[i+1]-revenue[i])
+
+
+# In[6]:
+
+
+#Set Minimum and Maximum Revenue Changes
+max_increase_revenue = max(revenue_change)
+max_decrease_revenue = min(revenue_change)
+
+
+# In[7]:
+
+
+#Max and Min Increase Indexes
+max_increase_month = revenue_change.index(max(revenue_change)) + 1
+max_decrease_month = revenue_change.index(min(revenue_change)) + 1
+
+
+# In[8]:
+
+
+#Print Financial Analysis Statements 
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(month)}")
+print(f"Total: ${sum(revenue)}")
+print(f"Average Change: ${round(sum(revenue_change)/len(revenue_change),2)}")
+print(f"Greatest Increase in Profits: {month[max_increase_month]} (${(str(max_increase_revenue))})")
+print(f"Greatest Decrease in Profits: {month[max_decrease_month]} (${(str(max_decrease_revenue))})")
+
+
+# In[9]:
+
+
+#Actions to Place Onto Text File
+output = Path("..", "PyBank", "Financial_Analysis_Summary.txt")
+
+with open(output,"w") as file:
+    
+
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("----------------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(month)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(revenue)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(revenue_change)/len(revenue_change),2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {month[max_increase_month]} (${(str(max_increase_revenue))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {month[max_decrease_month]} (${(str(max_decrease_revenue))})")
+
